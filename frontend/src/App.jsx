6817c05fd6e3febd13c69b292746a74b0030d9e4
ChandranSukirthan <sukirsukirthan21@gmail.com>
2026-06-20 10:06:58 +0530

    modify App.jsx and Auth page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,27 @@ import { ThemeContext } from './context/theme';
 import { ThemeProvider } from './context/ThemeContext';
 import { WallpaperProvider } from './context/WallpaperContext';
 import { Route, Routes } from 'react-router';
+import ChatPage from "./pages/ChatPage"
+import AuthPage from "./pages/AuthPage"
+import {useAuth} from "@clerk/react"
 
 function App() {
+  const {isSignedIn, isLoaded} = useAuth();
+
+  //todo: make this a better component
+  if(!isLoaded) return <p>loading...</p>
+
+  const {isSignedIn, isLoaded} = useAuth();
+
   return(
     <ThemeProvider>
       <WallpaperProvider>
       <Routes>
 
-      <Route path="/" element={<ChatPage/>}/>
-      <Route path="/auth" element={<AuthPage/>}/>
+      <Route path="/" element={isSignedIn ? <ChatPage/> : <Navigate to={"/auth"} repalce />}/>
+      <Route path="/auth" element={!isSignedIn ? <AuthPage/> : <Navigate to={"/auth"} repalce />}/>
 
-
+      </Routes>
       </WallpaperProvider>
       </ThemeProvider>
   );
